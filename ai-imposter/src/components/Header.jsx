8d78a964cf39e909681f490816daf3b49a0a62a2
TuvalZit18 <18tuvalzit18@gmail.com>
2026-07-09 01:19:12 +0300

    Creating Header Commit

---
+++ ai-imposter/src/components/Header.jsx
@@ -0,0 +1,60 @@
+import Classes from "../styles/Header.module.css";
+import logo from "../assets/images/AI-imposter_logo.png";
+import { DoorClosedLocked, UsersRound } from "lucide-react";
+import Timer from "./Timer";
+import { Flex, Text } from "@mantine/core";
+import GameRoundStatus from "./GameRoundStatus";
+
+const Header = ({ gameStatus = "PLAYING" }) => {
+  return (
+    <header className={Classes["header-wrapper"]}>
+      <Flex className={Classes["logo-container"]}>
+        <img className={Classes["header-logo"]} src={logo} alt="AI Imposter" />
+      </Flex>
+      <div className={Classes["verticalDivider"]}></div>
+
+      <Flex className={Classes["room-container"]}>
+        <Flex className={Classes["room-info-container"]}>
+          <Text span size="xl" className={Classes["room-label"]}>
+            Room Code
+          </Text>
+          <Text span size="xl" className={Classes["room-code"]}>
+            {"[784392]"}
+          </Text>
+        </Flex>
+        <DoorClosedLocked className={Classes["room-icon"]} stroke="white" />
+      </Flex>
+
+      <div className={Classes["verticalDivider"]}></div>
+      {gameStatus === "WAITING" && (
+        <Flex className={Classes["waitingroom-container"]}>
+          <Flex className={Classes["waitingroom-title"]}>
+            <UsersRound
+              size={"4rem"}
+              className={Classes["header-icon"]}
+              stroke="var(--primary)"
+            />
+            <Text size="xl" className={Classes["waitingroom-label"]}>
+              Waiting Room
+            </Text>
+          </Flex>
+          <Flex
+            className={`${Classes["status-container"]} ${Classes["waiting"]}`}
+          >
+            <Text span size="xl" className={`${Classes["status-text"]}`}>
+              Waiting
+            </Text>
+          </Flex>
+        </Flex>
+      )}
+      {gameStatus === "PLAYING" && (
+        <GameRoundStatus completedRounds={1} totalRounds={5} />
+      )}
+
+      <div className={Classes["verticalDivider"]}></div>
+      <Timer />
+    </header>
+  );
+};
+
+export default Header;
