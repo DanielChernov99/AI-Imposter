5a267f46c16d22826ca52f642912f6b508b6c46c
Daniel Chernov <chernov.dani@gmail.com>
2026-07-11 17:03:05 +0300

    added basic Routes

--- ai-imposter/src/App.jsx
+++ ai-imposter/src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router";
+import StartPage from "./pages/StartPage";
+import LobbyPage from "./pages/LobbyPage";
+
+import GamePage from "./pages/GamePage";
+import ResultPage from "./pages/ResultPage";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<StartPage />}></Route>
+      <Route path="/lobby" element={<LobbyPage />}></Route>
+      <Route path="/game" element={<GamePage />}></Route>
+      <Route path="/result" element={<ResultPage />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
